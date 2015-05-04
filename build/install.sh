010308f8fab6d48f5d0e75f2cc422cf110d14e2a
Forest Mars <forestmars+github@gmail.com>
2015-05-04 03:04:37 -0400

    install script

--- build/install.sh
+++ build/install.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-set -e
-path=$(dirname "$0")
-true=`which true`
-source $path/common.sh
+# set -e
+# path=$(dirname "$0")
+# source $path/common.sh
 
-echo "Installing Drupal minimal profile."
-echo "Installing site...";
-sqlfile=$base/build/ref/$PROJECT.sql
-gzipped_sqlfile=$sqlfile.gz
-if [ -e "$gzipped_sqlfile" ]; then
-  echo "...from reference database."
-  $drush sql-drop -y
-  zcat "$gzipped_sqlfile" | $drush sqlc
-elif [ -e "$sqlfile" ]; then
-  echo "...from reference database."
-  $drush sql-drop -y
-  $drush sqlc < $sqlfile
-else
-  echo "...from scratch, with Drupal minimal profile.";
-# Setting PHP Options so that we don't fail while sending mail if a mail sytem
-# doesn't exist.
-  PHP_OPTIONS="-d sendmail_path=`which true`" $drush si minimal --account-name=admin --account-pass=drupaladm1n
-fi
-source $path/update.sh
+# git clone ssh://codeserver.dev.33bc5703-7175-4ab6-b922-1147b8121e81@codeserver.dev.33bc5703-7175-4ab6-b922-1147b8121e81.drush.in:2222/~/repository.git www
+time composer -vvv install
+
+#chmod -R +w $base/cnf
+#chmod -R +w $base/www/sites/default
--- build/update.sh
+++ build/update.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-set -e
-path=$(dirname "$0")
-source $path/common.sh
+# set -e
+# path=$(dirname "$0")
+# source $path/common.sh
 
-echo "Enabling modules";
-$drush en $(echo $DROPSHIP_SEEDS | tr ':' ' ')
-echo "Rebuilding registry and clearing caches.";
-$drush rr
-$drush cc drush
-echo "Running manifests"
-$drush kw-m
-echo "Set default theme";
-$drush scr $base/build/scripts/default_set_theme.php
-echo "Clearing caches one last time.";
-$drush cc all
 
-chmod -R +w $base/cnf
-chmod -R +w $base/www/sites/default
+rm -rf core/.git
+time composer update
+#chmod -R +w $base/cnf
+#chmod -R +w $base/www/sites/default
